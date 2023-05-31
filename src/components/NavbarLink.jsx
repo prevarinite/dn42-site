0a607bbd4ecfc92563c2dda8a12f29069da92bcb
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-05-31 19:00:14 +0100

    Added icons to navbar links

--- src/components/NavbarLink.jsx
+++ src/components/NavbarLink.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import './NavbarLink.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShuffle, faMap, faMagnifyingGlass, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(faShuffle, faMap, faMagnifyingGlass, faTriangleExclamation);
 
 export default class NavbarLink extends Component {
     render() {
         return (
             <a href={this.props.url} className="navbar-link">
-                {/*{this.props.icon !== undefined && (<i className={this.props.icon} style={this.props.style}/>)}*/}
-                {this.props.name}
+                {this.props.icon !== undefined && (<FontAwesomeIcon icon={this.props.icon} />)}
+                &nbsp;&nbsp;{this.props.name}
             </a>
         )
     }
